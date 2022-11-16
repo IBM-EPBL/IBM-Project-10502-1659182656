62185e47212c4aaafd0a80b6bfdd64d61b9e90aa
Bhavika Arage <85723109+BhauArage@users.noreply.github.com>
2022-11-16 20:01:37 +0530

    File restructure

--- Project Development Phase/Sprint 4/Personal-Expense-Tracker-ibm/static/displaying.js
+++
@@ -1,9 +0,0 @@
-function displayingDiv(element) {
-    let el = document.getElementById(element);
-    if (el.style.display == "none") {
-        el.style.display = "block"
-    }
-    else {
-        el.style.display = "none"
-    }
-}
